508ff3fdf21e60248d0085286e36ee07e4f343b1
TravisLovingood <61667953+TravisLovingood@users.noreply.github.com>
2020-08-02 01:31:05 -0400

    big changes to App.js,footer.js,and contact.js

--- src/App.js
+++ src/App.js
@@ -17,23 +17,25 @@ class App extends Component {
     return (
       <HashRouter basename="/">
         <div>
-          <Container>
-            <h1>Travis Lovingood</h1> 
-          <Image src="links/Me.jpeg" roundedCircle alt="Picture of Travis"/>
-          </Container> 
-          <ul>
+
+            <h1>Travis Lovingood</h1>     
+             <h4>Web Developer</h4> 
+             <hr />
+
+          <div ClassName= "card">
             <li><Link to="/">Home </Link></li>
             <li><Link to="/portfolio">Portfolio </Link></li>
             <li><Link to="/contact">Contact </Link></li>
-          </ul>
-
+          </div>  
           <hr />
-            <h2>Web Developer</h2>
-
+          <Image src="links/Me.jpeg" roundedCircle alt="Picture of Travis"/>
+          <hr />
           <Route exact path="/" component={Home} />
           <Route path="/portfolio" component={Portfolio} />
           <Route path="/contact" component={Contact} />
         </div>
+
+        <hr />
         <Footer/>
       </HashRouter>
     );
@@ -48,17 +50,17 @@ const Home = () =>
           <div className="card indigo lighten-1">
             <div className="card-action">
               <p>
-              Greetings, I am a newly graduated web developer offering knowledge and experience in various programming languages such as HTML, CSS, JQuery, and
-              JavaScript. I am skilled in translating solutions into code and responding to challenges by developing solutions, as well as designing web
+              I am a newly graduated web developer offering knowledge and experience in various programming languages such as HTML, CSS, JQuery,
+              JavaScript, and React. I respond to challenges by developing solutions, I am skilled in translating solutions into code as well as designing web
               applications for desktop, phone, and mobile app users. Please check out my portfolio and my links.
               </p>
             </div>
             <div className="card-action">
-              <ul>
+
                 <li><a href="links/resume.docx">Resume </a></li>
                 <li><a href="https://www.linkedin.com/in/travis-lovingood-8868101a7/">LinkedIn </a></li>
                 <li><a href="https://github.com/TravisLovingood">Github</a></li>
-              </ul>
+
             </div>
           </div>
         </div>  
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,19 +1,31 @@
 import React from "react";
 
-// import Col from "react-bootstrap/Col";
-// import Row from"react-bootstrap/Row";
-// import Navbar from "../component/Navbar";
-
-
+import "../App.css"
 
 const Contact = () => {
-    return(
-    
-    <div>hello Contact</div>
-        
-        
-
+    return(     
+        <div className="row">
+            <div className="col s12"></div>
+        <div className="row">
+            <div className="container">
+                <div className="col s12">
+                    <div className="card">
+                        <h4><strong>Please leave your email.</strong></h4>
+                        <div className="card-action">
+                            <div className="row">
+                                <div className="input-field col s12">                                    
+                                    <input id="email" type="email" className="validate"></input>
+                                    <button type="submit" id="emailSaveButton">Submit</button>
+                                </div>
+                              </div>  
+                            </div>
+                        </div>
+                    </div>
+                </div>    
+            </div>
+        <div className="col s12"></div>
+        </div>
     )
 }
 
-export default Contact;
+export default Contact; 
