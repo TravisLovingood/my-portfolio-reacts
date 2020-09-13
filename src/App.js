6b464132f4acc58d31429b3c2c026029f8d1a4c4
TravisLovingood <61667953+TravisLovingood@users.noreply.github.com>
2020-09-13 16:09:31 -0400

    full changge started

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { HashRouter, Route, Link } from "react-router-dom";
 
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
-
+import Bio from './pages/Bio';
 import Footer from "./component/footer";
 
 
@@ -25,60 +25,46 @@ class App extends Component {
             </div>
             <div className="col 1"></div> 
           </div>
-    
-            
-             <hr />
 
           <div ClassName= "card">
-            <li><Link to="/">About </Link></li>
+            <li><Link to="/">Home </Link></li>
+            <li><Link to="/bio">Bio </Link></li>
             <li><Link to="/portfolio">Portfolio </Link></li>
             <li><Link to="/contact">Contact </Link></li>
           </div>  
           <hr />
 
-
-
-          <Route exact path="/" component={About} />
+          <Route exact path="/" component={Home} />
+          <Route path="/bio" component={Bio} />
           <Route path="/portfolio" component={Portfolio} />
           <Route path="/contact" component={Contact} />
         </div>
 
-        <hr />
-
-           <Footer/>
-
-       
+           
+           
+        <Footer/>
       </HashRouter>
+      
     );
   }
 }
 
-const About = () => 
-
-
-          <div className ="container">
-            <div className="row">
-              <div className="col s12"> 
-                <div className="card">
-                  <div className="card-action">          
-                    <Image src="links/Me.jpeg" rounded alt="Picture of Travis"/>
-                    <p>
-                    I am a newly graduated web developer offering knowledge and experience in various programming languages such as HTML, CSS, JQuery,
-                    JavaScript, and React. I respond to challenges by developing solutions, I am skilled in translating solutions into code as well as designing web
-                    applications for desktop, phone, and mobile app users. Please check out my portfolio and my links.
-                    </p>
-                  </div>
-                  <div className="card-action">
-                    <li><a href="links/resume.docx">Resume </a></li>
-                    <li><a href="https://www.linkedin.com/in/travis-lovingood-8868101a7/">LinkedIn </a></li>
-                    <li><a href="https://github.com/TravisLovingood">Github</a></li>
-                  </div> 
-                </div>
-              </div>    
-            </div>
-          </div> 
-
-
-
+const Home = () => 
+  <div className="row">   
+    <div className= "col s1"></div>
+    <div className= "col s10">
+      <div className="card">
+        <div className="card-action">          
+          <Image src="links/Me.jpeg" rounded alt="Picture of Travis"/>
+        </div>
+        <div className="card-action">
+          <li><a href="links/resume.docx">Resume </a></li>
+          <li><a href="https://www.linkedin.com/in/travis-lovingood-8868101a7/">LinkedIn </a></li>
+          <li><a href="https://github.com/TravisLovingood">Github</a></li>
+        </div> 
+      </div>
+    </div> 
+    <div className= "col s1"></div>
+  </div>
 
 export default App;
